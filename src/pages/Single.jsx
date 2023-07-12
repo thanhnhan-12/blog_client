c794d11ec5977bb92361eb3b3718d1d5ff6a36b8
unknown <nguyenthanhnhan12701@gmail.com>
2023-07-12 21:37:01 +0700

    Single

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import Edit from '../img/edit.png'
+import Delete from '../img/delete.png'
+import { Link } from 'react-router-dom'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className="content">
+        <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      
+        <div className="user">
+          <img src="https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        
+          <div className="info">
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+
+          <div className="edit">
+            <Link to={`write?edit=2`}>
+              <img src={Edit} alt="" />
+            </Link>
+            <Link>
+              <img src={Delete} alt="" />
+            </Link>
+          </div>
+        </div>
+
+        <h1>Lorem ipsum dolor sit, amet consectetur.</h1>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deleniti ea iure laborum delectus consequatur, neque assumenda. Quis, dolorum qui dicta voluptatibus fugit nemo, harum deleniti officia laboriosam adipisci pariatur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deleniti ea iure laborum delectus consequatur, neque assumenda. Quis, dolorum qui dicta voluptatibus fugit nemo, harum deleniti officia laboriosam adipisci pariatur.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident deleniti ea iure laborum delectus consequatur, neque assumenda. Quis, dolorum qui dicta voluptatibus fugit nemo, harum deleniti officia laboriosam adipisci pariatur.
+        </p>
+
+      </div>
+
+
+      <div className="menu">
+        m
+      </div>
+    </div>
   )
 }
 
