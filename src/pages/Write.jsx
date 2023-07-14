a8f8e7ed5182a0ff6217d9bf2cbbeaa35d802d01
unknown <nguyenthanhnhan12701@gmail.com>
2023-07-14 18:48:14 +0700

    Register

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
@@ -8,22 +8,33 @@ const Write = () => {
   console.log(value);
 
   return (
-    <div className='add'>
+    <div className="add">
       <div className="content">
-        <input type="text" placeholder='Title' />
+        <input type="text" placeholder="Title" />
         <div className="editorContainer">
-          <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
+          <ReactQuill
+            className="editor"
+            theme="snow"
+            value={value}
+            onChange={setValue}
+          />
         </div>
       </div>
 
       <div className="menu">
         <div className="item">
           <h1>Publish</h1>
-          <span><b>Status: </b> Draft</span>
-          <span><b>Visibility: </b> Public</span>
-          
-          <input style={{display: "none"}} type="file" name="" id="file" />
-          <label className='file' htmlFor="file">Upload Image</label>
+          <span>
+            <b>Status: </b> Draft
+          </span>
+          <span>
+            <b>Visibility: </b> Public
+          </span>
+
+          <input style={{ display: 'none' }} type="file" name="" id="file" />
+          <label className="file" htmlFor="file">
+            Upload Image
+          </label>
 
           <div className="buttons">
             <button>Save as a draft</button>
@@ -33,28 +44,40 @@ const Write = () => {
 
         <div className="item">
           <h1>Category</h1>
-          <input type="radio" name='cat' value="art" id='art' />
-          <label htmlFor="art">Art</label>
 
-          <input type="radio" name='cat' value="science" id='science' />
-          <label htmlFor="science">Science</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="art" id="art" />
+            <label htmlFor="art">Art</label>
+          </div>
 
-          <input type="radio" name='cat' value="technology" id='technology' />
-          <label htmlFor="technology">Technology</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="science" id="science" />
+            <label htmlFor="science">Science</label>
+          </div>
 
-          <input type="radio" name='cat' value="cinema" id='cinema' />
-          <label htmlFor="cinema">Cinema</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="technology" id="technology" />
+            <label htmlFor="technology">Technology</label>
+          </div>
 
-          <input type="radio" name='cat' value="design" id='design' />
-          <label htmlFor="design">Design</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="cinema" id="cinema" />
+            <label htmlFor="cinema">Cinema</label>
+          </div>
 
-          <input type="radio" name='cat' value="food" id='food' />
-          <label htmlFor="food">Food</label>
+          <div className="cat">
+            <input type="radio" name="cat" value="design" id="design" />
+            <label htmlFor="design">Design</label>
+          </div>
 
+          <div className="cat">
+            <input type="radio" name="cat" value="food" id="food" />
+            <label htmlFor="food">Food</label>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
